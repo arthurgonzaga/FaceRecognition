9e70b10b430c9c54c98a55f632da7a5556029261
Arthur Gonzaga <arthurbilul@gmail.com>
2020-02-28 21:07:16 -0300

    deleting pics after training

--- _docs/computer/train.py
+++ _docs/computer/train.py
@@ -13,76 +13,98 @@ import cv2
 import numpy as np
 import sys
 
-
-# Loading files
-face_detector = dlib.get_frontal_face_detector()
-shape_predictor = dlib.shape_predictor("resources/shape_predictor_68_face_landmarks.dat")
-face_recognition = dlib.face_recognition_model_v1("resources/dlib_face_recognition_resnet_model_v1.dat")
-
-# Input this value
-classroomID = '1A' #sys.argv[1]
-
-# Inicializing variables
-names = {}
-descriptors = None
-index = 0
-
-# File paths according to the classroom ID
-trainFolderPath = "classrooms/{}/train/".format(classroomID)
-descritorPath = "classrooms/{}/descriptor.npy".format(classroomID)
-namesPath = "classrooms/{}/names.pickle".format(classroomID)
-
-
-# Loop through all files in the train folder
-for file in glob.glob(os.path.join(trainFolderPath, "*.jpg")):
+def start(_id, name):
+    # Loading files
+    face_detector = dlib.get_frontal_face_detector()
+    shape_predictor = dlib.shape_predictor("resources/shape_predictor_68_face_landmarks.dat")
+    face_recognition = dlib.face_recognition_model_v1("resources/dlib_face_recognition_resnet_model_v1.dat")
     
-    # Read the file as a picture
-    image = cv2.imread(file)
+    # Input this value
+    classroomID = _id
     
-    # Detect faces in the image (array)
-    facesDetected = face_detector(image, 1)
-    print(len(facesDetected))
+    # Inicializing variables
+    names = {}
+    descriptors = None
+    index = 0
+    lastTrain = False
     
-    # Verify if is there just one face in the array
-    if(len(facesDetected) != 1):
-        print("Something is wrong with the file: {}".format(file))
-        break
-
-    # Get the face
-    face = facesDetected[0]
+    # File paths according to the classroom ID
+    trainFolderPath = "classrooms/{}/train/".format(classroomID)
+    descriptorsPath = "classrooms/{}/descriptors.npy".format(classroomID)
+    namesPath = "classrooms/{}/names.pickle".format(classroomID)
     
-    # Generate the 128D Vector from the face
-    points = shape_predictor(image, face)
+    try:
+        names = np.load(namesPath)
+        index = len(names)
+        descriptors = np.load(descriptorsPath)
+        lastTrain = True
+    except:
+        lastTrain = False
+        print('No train, let\'s start')
     
-    # Generate the face descriptor from the points
-    faceDescriptor = face_recognition.compute_face_descriptor(image, points)
-    
-    # Organize in a list
-    faceDescriptorsList = [fd for fd in faceDescriptor]
-    
-    # Create a npArray from the list
-    npArray = np.asarray(faceDescriptorsList, dtype=np.float64)
-    
-    # Create a new axis
-    npArray = npArray[np.newaxis, :]
-    
-    # Verify if descriptors is already inicialized or not
-    if descriptors is None:
-        descriptors = npArray
-    else:
-        # Concatenate the descriptor from the current face in the descriptors array
-        descriptors = np.concatenate((descriptors, npArray), axis=0)
+    # Loop through all files in the train folder
+    for file in glob.glob(os.path.join(trainFolderPath, "*.jpg")):
+        print(index)
+        # Read the file as a picture
+        image = cv2.imread(file)
         
-    # Add the name of the file in the names array
-    names[index] = os.path.basename(file).split(".")[0]
-    print(names[index])
+        # Detect faces in the image (array)
+        facesDetected = face_detector(image, 1)
+        #print(len(facesDetected))
+        
+        # Verify if is there just one face in the array
+        if(len(facesDetected) != 1):
+            print("Something is wrong with the file: {}".format(file))
+            break
     
-    # Increase index
-    index += 1
-
-
-# When the loop finish
-# It save the descriptors and the names in these paths
-np.save(descritorPath, descriptors)
-with open(namesPath, 'wb') as file:
-	cPickle.dump(names, file)
+        # Get the face
+        face = facesDetected[0]
+        
+        # Generate the 128D Vector from the face
+        points = shape_predictor(image, face)
+        
+        # Generate the face descriptor from the points
+        faceDescriptor = face_recognition.compute_face_descriptor(image, points)
+        
+        # Organize in a list
+        faceDescriptorsList = [fd for fd in faceDescriptor]
+        
+        # Create a npArray from the list
+        npArray = np.asarray(faceDescriptorsList, dtype=np.float64)
+        
+        # Create a new axis
+        npArray = npArray[np.newaxis, :]
+        
+        # Verify if descriptors is already inicialized or not
+        if descriptors is None:
+            descriptors = npArray
+        else:
+            # Concatenate the descriptor from the current face in the descriptors array
+            descriptors = np.concatenate((descriptors, npArray), axis=0)
+            
+        # Add the name of the file in the names array
+        names[index] = os.path.basename(file).split(".")[0]
+        #print(names[index])
+        
+        # Increase index
+        index += 1
+    
+    
+    # When the loop finish
+        
+    # Delete the old training files
+    if(lastTrain == True):
+        os.remove(namesPath)
+        os.remove(descriptorsPath)
+        
+    # Delete the pictures used to train
+    tempIndex = 5
+    while(tempIndex >= 0):
+        os.remove('{}{}.{}.jpg'.format(trainFolderPath, name, tempIndex))
+        tempIndex -= 1
+    
+    # It save the descriptors and the names in these paths
+    np.save(descriptorsPath, descriptors)
+    with open(namesPath, 'wb') as file:
+    	cPickle.dump(names, file)
+        
